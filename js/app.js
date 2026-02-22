8069f2bce548fd45329ae254e1b17c524b886c99
Anton Klimenko <anton1klasbk.ru@MacBook-Air-Anton.local>
2026-02-22 23:53:35 +0800

    Wishbone

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+;// CONCATENATED MODULE: ./src/js/files/functions.js
+function menuOpen() {
+  window.addEventListener("load", function () {
+    document.addEventListener("click", function (e) {
+      const targetElement = e.target;
+      if (!targetElement.closest(".icon-menu")) return;
+      if (targetElement.closest(".icon-menu")) {
+        document.documentElement.classList.toggle("menu-open");
+        document.documentElement.classList.toggle("lock");
+        targetElement.setAttribute("aria-label", "Open menu");
+        if (document.documentElement.classList.contains("menu-open")) {
+          targetElement.setAttribute("aria-label", "Close menu");
+        }
+      }
+    });
+  });
+}
+;// CONCATENATED MODULE: ./src/js/app.js
+
+menuOpen();
+
+/******/ })()
+;
